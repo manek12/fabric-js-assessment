a57710dd88e47696a3fafd1ad01b837e96279acf
Vivek Tyagi <vt2852241@gmail.com>
2022-07-26 08:12:21 +0530

    zoom in zoom out updated

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-var canvas = new fabric.Canvas('canvas');
+var canvas = new fabric.Canvas('canvas',{selection:false});
+
+let viewports = [];
+let uploadedImage = null;
+let dragable = {
+  dragableLeft: false,
+  dragableTop: false,
+  dragableRight: false,
+  dragableBottom: false,
+};
 
 document.getElementById("uploader").onchange = function(e) {
   var reader = new FileReader();
@@ -7,37 +16,138 @@ document.getElementById("uploader").onchange = function(e) {
     image.src = e.target.result;
     image.onload = function() {
       var img = new fabric.Image(image);
-      img.set({
-        
-      });
+      uploadedImage=img;
       img.scaleToWidth(800);
       img.scaleToHeight(600);
-      canvas.clear();
-      canvas.add(img).setActiveObject(img).renderAll();
+      img.selectable=false;
+      canvas.clear().renderAll();
+      canvas.add(img);
       canvas.centerObject(img);
-      console.log(canvas);
       
     }
   }
   reader.readAsDataURL(e.target.files[0]);
 }
 
-canvas.on('mouse:wheel', function(opt) {
-    var delta = opt.e.deltaY;
-    var zoom = canvas.getZoom();
-    console.log(canvas.viewportTransform);
-    zoom *= 0.999 ** delta;
-    if (zoom > 30) zoom = 30;
-    if (zoom < 1){
-        zoom = 1;
-        canvas.setViewportTransform([1,0,0,1,0,0]);
+canvas.on("mouse:wheel", function (options) {
+  let delta = options.e.deltaY;
+  let presentZoom = canvas.getZoom();
+  let newZoom = presentZoom * Math.pow(0.9995, delta);
+  let vpt = this.viewportTransform;
+
+  setImageDragableProperty();
+  if (newZoom >= presentZoom) {
+    newZoom = newZoom < 0.1 ? 0.1 : newZoom;
+    viewports.push(vpt);
+    canvas.zoomToPoint({ x: options.e.offsetX, y: options.e.offsetY }, newZoom);
+  } else {
+    newZoom = newZoom > 20 ? 20 : newZoom;
+    if (viewports.length > 0) {
+      let previousViewport = viewports[viewports.length - 1];
+      viewports.pop();
+      canvas.setViewportTransform(previousViewport);
     }
-    canvas.zoomToPoint(new fabric.Point(opt.e.offsetX,opt.e.offsetY),zoom);
-    opt.e.preventDefault();
-    opt.e.stopPropagation();
-})
+  }
+});
+
+canvas.on("mouse:down", function (opt) {
+  var evt = opt.e;
+  if (evt.altKey === true) {
+    this.isDragging = true;
+    this.selection = false;
+    this.lastPosX = evt.clientX;
+    this.lastPosY = evt.clientY;
+  }
+});
+canvas.on("mouse:move", function (options) {
+  if (this.isDragging && viewports.length > 0) {
+    var e = options.e;
+    var vpt = this.viewportTransform;
+    console.log(vpt);
+    if (dragable.dragableLeft || dragable.dragableRight)
+      vpt[4] += e.clientX - this.lastPosX;
+    if (dragable.dragableTop || dragable.dragableRight)
+      vpt[5] += e.clientY - this.lastPosY;
+
+    this.requestRenderAll();
+    this.lastPosX = e.clientX;
+    this.lastPosY = e.clientY;
+  }
+});
+canvas.on("mouse:up", function () {
+  this.setViewportTransform(this.viewportTransform);
+  this.isDragging = false;
+  this.selection = true;
+});
 
 
 function chooseFile(){
     document.getElementById('uploader').click();
 }      
+
+
+
+//smoothing
+
+const getCanvasCenterCornerCoordinates = function (points) {
+  var invertedMatrix = fabric.util.invertTransform(canvas.viewportTransform);
+  var transformedP = fabric.util.transformPoint(points, invertedMatrix);
+  return transformedP;
+};
+const setDragable = function (
+  canvasBottomCornerCoordinates,
+  canvasTopCornerCoordinates,
+  canvasRightCornerCoordinates,
+  canvasLeftCornerCoordinates
+) {
+  dragable.dragableBottom =
+    canvasBottomCornerCoordinates.y <
+    (canvasBottomCornerCoordinates.y - canvasTopCornerCoordinates.y) / 2 +
+      uploadedImage.height / 2
+      ? true
+      : false;
+
+  dragable.dragableTop =
+    canvasTopCornerCoordinates.y >
+    (canvasBottomCornerCoordinates.y - canvasTopCornerCoordinates.y) / 2 -
+      uploadedImage.height / 2
+      ? true
+      : false;
+
+  dragable.dragableRight =
+    canvasRightCornerCoordinates.x <
+    (canvasRightCornerCoordinates.x - canvasLeftCornerCoordinates.x) / 2 +
+      uploadedImage.width / 2
+      ? true
+      : false;
+  dragable.dragableLeft =
+    canvasLeftCornerCoordinates.x >
+    (canvasRightCornerCoordinates.x - canvasLeftCornerCoordinates.x) / 2 -
+      uploadedImage.width / 2
+      ? true
+      : false;
+};
+const setImageDragableProperty = function () {
+  let canvasBottomCornerCoordinates = getCanvasCenterCornerCoordinates({
+    x: canvas.width / 2,
+    y: canvas.height,
+  });
+  let canvasTopCornerCoordinates = getCanvasCenterCornerCoordinates({
+    x: canvas.width / 2,
+    y: 0,
+  });
+  let canvasRightCornerCoordinates = getCanvasCenterCornerCoordinates({
+    x: canvas.width,
+    y: canvas.height / 2,
+  });
+  let canvasLeftCornerCoordinates = getCanvasCenterCornerCoordinates({
+    x: 0,
+    y: canvas.height / 2,
+  });
+  setDragable(
+    canvasBottomCornerCoordinates,
+    canvasTopCornerCoordinates,
+    canvasRightCornerCoordinates,
+    canvasLeftCornerCoordinates
+  );
+};
